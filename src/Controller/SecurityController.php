a40bb0413874ae31b89797e5219a315c59ba0fe3
ilya <ilyaman5@gmail.com>
2019-09-04 10:23:41 +0000

    add security - controller and template

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="security")
+     */
+    public function index()
+    {
+        return $this->render('security/login.html.twig', [
+            'controller_name' => 'SecurityController',
+        ]);
+    }
+}
